c3d435a439e815ac79ae9b8788b47c54d1ccc0a4
308 <308@academicos.cesde.edu.co>
2022-03-10 20:57:09 -0500

    subir

--- feautures_login/login.php
+++ feautures_login/login.php
@@ -1,13 +1,15 @@
 <?php
-require_once(dirname(__DIR__)."/db/db_config.php");
+require_once(dirname(__DIR__)."../db/db_config.php");
 if($_SERVER['REQUEST_METHOD'] == "GET"){ 
 
     $identificacion=$_GET['identificacion'];
     $db= new DBConfig();
     $dbConnection = $db->connect();
-    $query = "SELECT * FROM user where identificacion=$identificacion";
-    $user = $dbConnection->query($query)->fetchAll();
-    print_r($user[0][0]);
+    $query = "SELECT * FROM user";
+    $user = $dbConnection->query($query)->fetchAll(PDO:: FETCH_ASSOC);
+
+    $age = array("Peter"=>"35", "Ben"=>"37", "Joe"=>"43");
+    echo(json_encode($user));
 } 
 else{ 
     echo "No se pudo"; 
